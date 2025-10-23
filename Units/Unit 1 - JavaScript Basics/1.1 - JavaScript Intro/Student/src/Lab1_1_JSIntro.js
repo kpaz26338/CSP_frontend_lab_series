a1b81d2220e4025fa2e3ff3791005993fcdf3dd5
kpaz26338 <kpaz26338@columbushs.com>
2025-10-23 11:37:59 -0400

    finished 1.1

--- Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
+++ Units/Unit 1 - JavaScript Basics/1.1 - JavaScript Intro/Student/src/Lab1_1_JSIntro.js
@@ -4,23 +4,40 @@
    =========================================================== */
 
 // TODO 1: Print your full name with console.log()
-
+console.log("Kevin Paz")
 
 // TODO 2: Declare variable favoriteFood, assign "Steak", then reassign "Pizza"
+let favoritefood = "Steak"; 
+favoritefood = "Pizza";
 
 
 // TODO 3: Create variable mystery, assign a string, then a number, print type each time
+let mystery = "Hello";
+console.log (typeof mystery);
+mystery = 42;
+console.log (typeof mystery) // number
+
 
 
 // TODO 4: Declare const schoolName, try to reassign
+const schoolname = "Christopher Columbus high school"
 
 
 // TODO 5: Create three valid camelCase variables and print them
+let quarterback;
+let pointGuard; // Declare
+
+pointGuard = "Dwyane Wade"; // Initialize
+let classroom = "L12" // Declare and Initialize
 
 
 // 🚀 Stretch Goals (optional)
 // - Create a variable age and print "I am X years old" using concatenation
 // - Repeat using template literals
+
+let age = 17; // Declared and Initialized
+console.log ("I am " + age + " years old");
+
 
 /* ===========================================================
    🎟 EXIT TICKET
@@ -28,3 +45,13 @@
    2. What challenges did you encounter, and how did you solve them?
    3. Where else could this programming concept be useful?
    =========================================================== */
+
+
+   let width = number (prompt ("Give me the width"))
+   let height = number( prompt ("Give me the height"));
+
+let area = width * height;
+let perimeter = 2 * (width + height);
+
+   console.log("Area:", area);
+   console.log("Perimeter:", perimeter);
